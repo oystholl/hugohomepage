a4968ed0b18b2c09b6b9817a87ff4e3519ac74e6
oystholl <oystholl@stud.ntnu.no>
2019-04-19 14:18:59 +0200

    Updated publishing script; upstream => origin

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -18,7 +18,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -27,4 +27,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push
